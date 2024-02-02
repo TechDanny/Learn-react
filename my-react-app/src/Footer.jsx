a910167126db4294a4c13164d62bf97c082bb7d4
root <kokidanny15@gmail.com>
2024-02-02 15:26:24 +0300

    removed files

--- my-react-app/src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return(
-        <footer>
-            &copy; {new Date().getFullYear()} Welcome to TechDanny
-        </footer>
-    )
-}
-export default Footer
--- my-react-app/src/Header.jsx
+++
@@ -1,8 +0,0 @@
-function Header() {
-    return (
-        <header>
-            <h1>My Website</h1>
-        </header>
-    )
-}
-export default Header
